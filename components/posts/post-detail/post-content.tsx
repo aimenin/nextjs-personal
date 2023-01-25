45f7d517106f2cd029958b3e914f4822ceeaf3c7
aimen <artem.igorevich8@rambler.ru>
2023-01-25 19:37:53 +0300

    add post content page

---
+++ components/posts/post-detail/post-content.tsx
@@ -0,0 +1,24 @@
+import PostHeader from './post-header';
+
+import classes from './post-content.module.css';
+
+const DUMMY_POST = {
+  slug: 'getting-started-with-nextjs',
+  title: 'Getting started with Nextjs',
+  image: 'getting-started-nextjs.png',
+  date: '2022-02-10',
+  content: '# This is a first post',
+};
+
+const PostContent = () => {
+  const imagePath = `/images/posts/${DUMMY_POST.image}`;
+
+  return (
+    <article className={classes.content}>
+      <PostHeader title={DUMMY_POST.title} image={imagePath} />
+      CONTENT
+    </article>
+  );
+};
+
+export default PostContent;
