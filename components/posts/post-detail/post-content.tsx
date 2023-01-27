88ce74814943bb8fe4ee9308f88cd6fa936756bd
aimen <artem.igorevich8@rambler.ru>
2023-01-27 20:12:21 +0300

    add custom renderer for images from md files

--- components/posts/post-detail/post-content.tsx
+++ components/posts/post-detail/post-content.tsx
@@ -1,9 +1,12 @@
 import ReactMarkdown from 'react-markdown';
+import Image from 'next/image';
+import { FC } from 'react';
 
 import PostHeader from './post-header';
 import classes from './post-content.module.css';
 import { getPostData } from '@/lib/posts-util';
-import { FC } from 'react';
+import { NormalComponents } from 'react-markdown/lib/complex-types';
+import { SpecialComponents } from 'react-markdown/lib/ast-to-react';
 
 const DUMMY_POST = {
   slug: 'getting-started-with-nextjs',
@@ -20,10 +23,25 @@ interface PostContentProps {
 const PostContent: FC<PostContentProps> = ({ post }) => {
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
+  const customRenderers: Partial<
+    Omit<NormalComponents, keyof SpecialComponents> & SpecialComponents
+  > = {
+    img(image) {
+      return (
+        <Image
+          src={image.src || ''}
+          alt={image.alt || 'markdown image'}
+          width={600}
+          height={300}
+        />
+      );
+    },
+  };
+
   return (
     <article className={classes.content}>
       <PostHeader title={post.title} image={imagePath} />
-      <ReactMarkdown>{post.content}</ReactMarkdown>
+      <ReactMarkdown components={customRenderers}>{post.content}</ReactMarkdown>
     </article>
   );
 };
