354d2d0229943b7bc1a44fd960e1c0dfffd2dfa4
aimen <artem.igorevich8@rambler.ru>
2023-02-05 20:19:27 +0300

    add connection to mongodb

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -1,4 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import { MongoClient } from 'mongodb';
 
 interface ContactNextApiRequest extends NextApiRequest {
   email: string;
@@ -6,7 +7,7 @@ interface ContactNextApiRequest extends NextApiRequest {
   message: string;
 }
 
-const handler = (req: ContactNextApiRequest, res: NextApiResponse) => {
+const handler = async (req: ContactNextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
     const { email, name, message } = req.body;
 
@@ -22,15 +23,34 @@ const handler = (req: ContactNextApiRequest, res: NextApiResponse) => {
       return;
     }
 
-    // Store in database
-
     const newMessage = {
       email,
       name,
       message,
     };
 
-    console.log('newMessage ', newMessage);
+    try {
+      const client = await MongoClient.connect(
+        `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.bllf1q7.mongodb.net/my-site?retryWrites=true&w=majority`
+      );
+
+      const db = client.db();
+
+      const result = await db.collection('messages').insertOne(newMessage);
+
+      const messageAfterStoring = {
+        ...newMessage,
+        _id: result.insertedId,
+      };
+
+      client.close();
+      res.status(201).json({
+        message: 'Successfully stored message!',
+        contactMessage: messageAfterStoring,
+      });
+    } catch (error) {
+      res.status(500).json({ message: 'Could not connect to database' });
+    }
   }
 };
 
