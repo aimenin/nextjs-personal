634642e674a4ceba2a5352b72a53450d062c1506
aimen <artem.igorevich8@rambler.ru>
2023-02-17 20:29:25 +0300

    add change password functionality to front part of our app

--- components/profile/profile-form.tsx
+++ components/profile/profile-form.tsx
@@ -1,16 +1,47 @@
-import { FC } from 'react';
+import { FC, FormEventHandler, useState } from 'react';
 import classes from './profile-form.module.css';
 
-const ProfileForm: FC = () => {
+export type PasswordInput = {
+  oldPassword: string;
+  newPassword: string;
+};
+
+export interface ProfileFormProps {
+  onChangePassword: (passwords: PasswordInput) => void;
+}
+
+const ProfileForm: FC<ProfileFormProps> = ({ onChangePassword }) => {
+  const [oldPassword, setOldPassword] = useState('');
+  const [newPassword, setNewPassword] = useState('');
+
+  const handleSubmit: FormEventHandler<HTMLFormElement> = (event) => {
+    event.preventDefault();
+
+    onChangePassword({
+      oldPassword,
+      newPassword,
+    });
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={handleSubmit}>
       <div className={classes.control}>
         <label htmlFor="new-password">New Password</label>
-        <input type="password" id="new-password" />
+        <input
+          type="password"
+          id="new-password"
+          value={newPassword}
+          onChange={(event) => setNewPassword(event.currentTarget.value)}
+        />
       </div>
       <div className={classes.control}>
         <label htmlFor="old-password">Old Password</label>
-        <input type="password" id="old-password" />
+        <input
+          type="password"
+          id="old-password"
+          value={oldPassword}
+          onChange={(event) => setOldPassword(event.currentTarget.value)}
+        />
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
--- components/profile/user-profile.tsx
+++ components/profile/user-profile.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { getSession } from 'next-auth/client';
 import { Session } from 'next-auth';
 
-import ProfileForm from './profile-form';
+import ProfileForm, { PasswordInput } from './profile-form';
 import classes from './user-profile.module.css';
 
 const UserProfile = () => {
@@ -22,10 +22,24 @@ const UserProfile = () => {
   //   return <p className={classes.profile}>Loading...</p>;
   // }
 
+  const changePasswordHandler = async (passwordData: PasswordInput) => {
+    const response = await fetch('/api/user/change-user', {
+      method: 'PATCH',
+      body: JSON.stringify(passwordData),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    const data = await response.json();
+
+    console.log('data ', data);
+  };
+
   return (
     <section className={classes.profile}>
       <h1>Your User Profile</h1>
-      <ProfileForm />
+      <ProfileForm onChangePassword={changePasswordHandler} />
     </section>
   );
 };
