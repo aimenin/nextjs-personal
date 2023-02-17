7f5ade6cdebf6c0790d3e53ed45adb40ac5c7642
aimen <artem.igorevich8@rambler.ru>
2023-02-17 20:15:36 +0300

    add functionality to change password api route

--- pages/api/user/change-user.ts
+++ pages/api/user/change-user.ts
@@ -1,7 +1,17 @@
+import { hashPassword, verifyPassword } from '@/lib/auth';
+import { connectToDatabase } from '@/lib/db';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { getSession } from 'next-auth/client';
 
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+interface ChangePasswordNextApiRequest extends NextApiRequest {
+  oldPassword: string;
+  newPassword: string;
+}
+
+const handler = async (
+  req: ChangePasswordNextApiRequest,
+  res: NextApiResponse
+) => {
   if (req.method !== 'PATCH') {
     return;
   }
@@ -12,6 +22,42 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     res.status(401).json({ message: 'Not authenticated' });
     return;
   }
+
+  const userEmail = session.user?.email;
+  const oldPassword = req.body.oldPassword;
+  const newPassword = req.body.newPassword;
+
+  const client = await connectToDatabase();
+
+  const usersCollection = client.db().collection('users');
+
+  const user = await usersCollection.findOne({ email: userEmail });
+
+  if (!user) {
+    res.status(404).json({ message: 'User not found' });
+    client.close();
+    return;
+  }
+
+  const currentPassword = user.password as string;
+
+  const isValid = await verifyPassword(oldPassword, currentPassword);
+
+  if (isValid) {
+    res.status(403).json({ message: 'You are not granted to change password' });
+    client.close();
+    return;
+  }
+
+  const hashedPassword = await hashPassword(newPassword);
+
+  const result = await usersCollection.updateOne(
+    { email: userEmail },
+    { $set: { password: hashedPassword } }
+  );
+
+  client.close();
+  res.status(200).json({ message: 'Password updated' });
 };
 
 export default handler;
